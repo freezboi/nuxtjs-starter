5e3dea33cb55eae4bd3b725a899cc520cfa359f2
freezboi <freezboi@gmail.com>
2019-11-18 18:38:26 +0900

    deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,9 +8,9 @@ rm -rf /home/ec2-user/front/app
 mkdir /home/ec2-user/front/app
 
 echo "> $ZIP_PATH 압축 해제"
-unzip -d /home/ec2-user/frontend/app/ $ZIP_PATH
+unzip -d /home/ec2-user/front/app/ $ZIP_PATH
 
-cd /home/ec2-user/frontend/app
+cd /home/ec2-user/front/app
 
 npm i
 
